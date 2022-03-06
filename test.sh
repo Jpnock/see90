4393961082592e3c1e5656dfc5bb501785938d9e
James Nock <jpnock@kryptoslogic.com>
2022-03-06 15:56:47 +0000

    test: Test using Docker

---
+++ test.sh
@@ -0,0 +1,11 @@
+set -euo pipefail
+
+make
+echo ".globl f" > ./test/all/main.s
+cat ./test/all/main.c | ./bin/see90 >> ./test/all/main.s
+
+mips-linux-gnu-gcc -mfp32 -o ./test/all/main.o -c ./test/all/main.s
+mips-linux-gnu-gcc -mfp32 -static -o ./test/all/main ./test/all/main.o ./test/all/root.c
+docker build -t see90 .
+docker run -v "$(pwd)/test/all":"/mnt/test" see90 /mnt/test/test.sh
+rm ./test/all/main
