55ebf95782792fc4cbcbb2481ad1fda9f3b38700
James Nock <jpnock@gmail.com>
2022-03-11 00:11:43 +0000

    cmd/see90: Remove comments from Lexer input

--- cmd/see90/main.go
+++ cmd/see90/main.go
@@ -2,12 +2,72 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/jpnock/see90/pkg/c90"
 )
 
+type CommentRemoverReader struct {
+	strippingMultiline  bool
+	strippingRestOfLine bool
+	r                   io.Reader
+	last                byte
+}
+
+func (r *CommentRemoverReader) Read(p []byte) (n int, err error) {
+	tmp := make([]byte, len(p)+1)
+
+	n, err = r.r.Read(tmp[1:])
+	if err != nil {
+		return n, err
+	}
+
+	if n == 0 {
+		return 0, nil
+	}
+
+	tmp[0] = r.last
+
+	for i := 0; i < n+1; i++ {
+		c := tmp[i]
+		var next byte
+		if i+1 < n {
+			next = tmp[i+1]
+		}
+
+		if r.strippingRestOfLine {
+			if c == '\n' {
+				r.strippingRestOfLine = false
+			} else {
+				tmp[i] = ' '
+			}
+		} else if r.strippingMultiline {
+			if c == '*' && next == '/' {
+				r.strippingMultiline = false
+				tmp[i+1] = ' '
+			}
+			tmp[i] = ' '
+		} else {
+			if c == '/' && next == '*' {
+				r.strippingMultiline = true
+				tmp[i] = ' '
+				tmp[i+1] = ' '
+			} else if c == '/' && next == '/' {
+				r.strippingRestOfLine = true
+				tmp[i] = ' '
+				tmp[i+1] = ' '
+			}
+		}
+	}
+
+	r.last = tmp[n-1]
+
+	copy(p, tmp[1:])
+	return n, nil
+}
+
 func main() {
-	c90.Parse(c90.NewLexer(os.Stdin))
+	c90.Parse(c90.NewLexer(&CommentRemoverReader{r: os.Stdin}))
 	fmt.Fprint(os.Stderr, c90.AST.Describe(0))
 }
--- test/all/pointers.c
+++ test/all/pointers.c
@@ -10,3 +10,20 @@ int f(int y, int z)
     f += 1;
     return f + **e + 5;
 }
+
+/*
+    int *my_func(x,y )
+
+    int *a = malloc(sizeof(int) * 10);
+    a[2] = 5;
+
+    *(a+2) = 5;
+
+    my_func(1, 3)[5] = 5;
+
+
+    int c = 5;
+    int *b = &c;
+    int **a = &b;
+    *a[5] = 6;
+*/
